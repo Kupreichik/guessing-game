8c6c9478e9ff0344fb1b86eec32dcceba6c87ed3
Julia Kupreichik <julilike@yandex.ru>
2022-08-08 21:20:34 +0300

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max, mid) {
+        this.min = min;
+        this.max = max;
+        this.mid = mid
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max
     }
 
     guess() {
-
+        return this.mid = Math.round((this.max - this.min) / 2 + this.min)
     }
 
     lower() {
-
+        this.max = this.mid
     }
 
     greater() {
-
+        this.min = this.mid
     }
 }
 
